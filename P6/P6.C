625e2bbe581d6188d646ace336c1cd708966592d
angel j. palacios g. <palacios.angel@uabc.edu.mx>
2026-03-13 23:09:58 +0000

    actual

--- P6/P6.C
+++ P6/P6.C
@@ -4,15 +4,54 @@
 
 #include<studio.h>
 #include<studib.h>
- 
+#include<studi.h>
+
 in main()
 
+int cantidaddeestudiantes(int estudiantes[]; int n)
+void leercalificaciones(int calificacion[]; int nota)
 float promedio
 float calificaciones
 int calificacionmamayor
 int calificaionmenor
 int numeros de reprobado
 int numero de aprobado 
-aprobado>=60
-reprobado<60
+aprobado==60
+
+{
+    //pedir datos 
+    
+    pritf("ingrese el numero de estudiante");
+    scant(n)
+    if(1<n<40);
+    else
+    pritf("el numero de estudiantes no esta permitido");
+
+
+    for(){
+
+    pritf("ingrese la calificacion del estudiante");
+    scant(calificacion);
+    if (0<nota<100)
+    else
+    pritf("esta calificacion no es permitida ");
+    }
+
+
+    //promedio
+
+
+    //calificacion mayor
+
+    //calificacion menor
+
+    //numero de reprobados
+
+    //numero de aprobados
+
+    //mostrar resultados 
+    return 0
+
+}
+
 
