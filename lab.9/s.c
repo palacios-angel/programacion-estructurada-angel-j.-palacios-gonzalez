6180dad78e0c6004137a0cb726673b219a75c41c
angel j. palacios g. <palacios.angel@uabc.edu.mx>
2026-04-17 11:32:36 -0700

    Actualizar s.c

--- lab.9/s.c
+++ lab.9/s.c
@@ -5,14 +5,14 @@
 
 int suma(int n);
 //principal
-main(){
+int main()
+{
 	int n=5;
 printf(suma es %\n", suma(n));
 reurnt 0;
 }
 //definicion de la funcion recursiva
-int suma(int n);{
-    int n=5;
+int suma(int n){
 
 //1. caso base:definir la recuersion cuando nes 0 o 1
 if (n <= 1){
